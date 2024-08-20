cac07362ac64580758f2394c7f402f8283b37b0f
Aynur <ainur2205@gmail.com>
2024-08-21 00:38:05 +0500

    Тестировал в браузере Chrome. В качестве локаторов использовал LINK_TEXT и CSS_SELECTOR

--- tests/test_first_scenario.py
+++ tests/test_first_scenario.py
@@ -1,2 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+"""
+    тест проводится в браузере Chrome
+"""
+
+
 def test_first_scenario():
-    pass
+    browser = webdriver.Chrome()
+    browser.get('https://sbis.ru/')
+
+    browser.find_element(By.LINK_TEXT, 'Контакты').click()
+    browser.find_element(By.CSS_SELECTOR, "a[href='https://tensor.ru/']").click()
