995ea82c1587e8ae35f3de4fe7252599e50451df
Aynur <ainur2205@gmail.com>
2024-08-31 21:18:03 +0500

    Промежуточные изменения

--- tests/test_second_scenario.py
+++ tests/test_second_scenario.py
@@ -24,3 +24,8 @@ def test_second_scenario():
     partners = list_partner.find_elements(By.CSS_SELECTOR, ".controls-ListView__itemV-relative.controls-ListView__itemV.controls-ListView__item_default.controls-ListView__item_contentWrapper.js-controls-ListView__editingTarget.controls-ListView__itemV_cursor-pointer.controls-ListView__item_showActions.js-controls-ListView__measurableContainer.controls-ListView__item__unmarked_default.controls-ListView__item_highlightOnHover.controls-hover-background-default.controls-Tree__item")
     assert len(partners) > 0
 
+    my_region_element[0].click()
+    time.sleep(10)
+    element = my_region_element[0].find_element(By.XPATH,"//li[@tabindex='0' and contains(@class, 'sbis_ru-Region-Panel__item')]//span[contains(text(), 'Камчатский край')]")
+    element.click()
+    time.sleep(10)
