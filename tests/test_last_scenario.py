b8748f0907e6ae9a42d9ff999c4494f1039bec9f
Aynur <ainur2205@gmail.com>
2024-09-02 21:49:00 +0500

    Промежуточные изменения

--- tests/test_last_scenario.py
+++ tests/test_last_scenario.py
@@ -1,7 +1,6 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 """
